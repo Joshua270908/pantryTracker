06fe7985f2c25994b2749500a5610fe3d567218e
Joshua270908 <128637180+Joshua270908@users.noreply.github.com>
2024-08-08 12:52:28 -0400

    q

--- app/page.js
+++ app/page.js
@@ -3,11 +3,41 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 import { firestore } from "@/firebase";
 import { Box, Typography } from "@mui/material";
+import { collection, query } from "firebase/firestore";
 
 export default function Home() {
+  const {inventory, setInventory} = useState([])
+  const { open, setOpen} = useState(false)
+  const {itemName, setItemName} = useState('')
+
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, 'inventory'))
+    const docs = await getDocs(snapshot)
+    const inventoryList = [];
+    docs.forEach((docs)=>{
+      inventoryList.push({
+        name: docs.id,
+        ...docs.data(),
+      })
+    })
+    setInventory(inventoryList)
+  }
+
+  useEffect(()=>{
+    updateInventory()
+  }, [])
+
   return (
     <Box>
-  <Typography variant="h1">Pantry tracker</Typography>
+  <Typography variant = "h1">Pantry tracker</Typography>
+  {inventory.forEach((item) => {
+      return (
+      <>
+      {item.name}
+      {item.count}
+      </>
+      )
+    })}
     </Box>
   )
 }
